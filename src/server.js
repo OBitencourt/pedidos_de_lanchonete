173f32dfcfbb7373f5ec421e5a42a40cfedfdd04
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-09-12 22:56:13 +0000

    configuring server and adding database

---
+++ src/server.js
@@ -0,0 +1,43 @@
+
+// requisitions-base
+const express = require('express')
+const cors = require('cors')
+
+// requisitions created
+
+
+const app = express()
+
+
+
+
+
+
+// configuring cors to make everyone able to do requests
+
+app.use(cors())
+
+
+// configuring server to receive datas in json
+
+app.use(express.json())
+
+
+// configuring routes
+
+
+
+
+
+// 404 error (not found)
+
+app.use('/', (req, res) => {
+    res.send('Página não encontrada!')
+})
+
+
+// server listening
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
